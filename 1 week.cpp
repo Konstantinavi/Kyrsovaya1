ef16922310574370cc8418b44b8eb8ec7b871b94
Матвеюк Антонина <Matvejk_AI_25@student.grsu.by>
2026-05-25 21:24:24 +0300

    Добавлена функция GetProcesses
    Реализован сбор данных о процессах и нагрузке системы

--- 1 week.cpp
+++ 1 week.cpp
@@ -86,3 +86,101 @@ void FreeProcessesArray() {
         processesCapacity = 0;
     }
 }
+INT GetProcesses() {
+    RefreshGuiProcessesList();
+    ClearProcessesData();
+
+    FILETIME sysIdle, sysKernel, sysUser;
+    static ULONGLONG lastSysKernel = 0, lastSysUser = 0;
+    ULONGLONG globalTimeDelta = 0;
+
+    if (GetSystemTimes(&sysIdle, &sysKernel, &sysUser)) {
+        ULONGLONG currentSysKernel = FileTimeToQuadWord(&sysKernel);
+        ULONGLONG currentSysUser = FileTimeToQuadWord(&sysUser);
+        globalTimeDelta = (currentSysKernel - lastSysKernel) + (currentSysUser - lastSysUser);
+
+        ULONGLONG currentSysIdle = FileTimeToQuadWord(&sysIdle);
+        static ULONGLONG lastSysIdle = 0;
+        if (lastSysIdle != 0 && globalTimeDelta > 0) {
+            ULONGLONG idleDelta = currentSysIdle - lastSysIdle;
+            globalCpuLoad = 100.0 - (idleDelta * 100.0 / globalTimeDelta);
+            if (globalCpuLoad < 0) globalCpuLoad = 0;
+            if (globalCpuLoad > 100) globalCpuLoad = 100;
+        }
+        lastSysIdle = currentSysIdle;
+        lastSysKernel = currentSysKernel;
+        lastSysUser = currentSysUser;
+    }
+    if (globalTimeDelta == 0) globalTimeDelta = 1;
+
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) return 0;
+
+    PROCESSENTRY32W pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+    globalNetSpeed = 0.0;
+    globalTotalThreads = 0;
+    globalTotalHandles = 0;
+
+    if (Process32FirstW(hSnapshot, &pe32)) {
+        do {
+            if (pe32.th32ProcessID == 0) continue;
+            EnsureProcessesCapacity(processesCount + 1);
+
+            ProcessInfo& proc = processes[processesCount];
+            proc.pid = pe32.th32ProcessID;
+
+            SIZE_T nameLength = wcslen(pe32.szExeFile) + 1;
+            proc.name = new WCHAR[nameLength];
+            wcscpy_s(proc.name, nameLength, pe32.szExeFile);
+
+            proc.category = DetectProcessCategory(pe32.th32ProcessID, pe32.szExeFile);
+            proc.cpuUsage = 0.0;
+            proc.memoryUsage = 0;
+            proc.diskUsage = 0.0;
+            proc.netUsage = 0.0;
+            proc.lastUpdateTime = GetTickCount64();
+
+            globalTotalThreads += pe32.cntThreads;
+
+            HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
+            if (hProcess != NULL) {
+                PROCESS_MEMORY_COUNTERS pmc;
+                if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+                    proc.memoryUsage = static_cast<DWORD>(pmc.WorkingSetSize);
+                }
+                FILETIME creationTime, exitTime, kernelTime, userTime;
+                if (GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime)) {
+                    ULONGLONG procKernel = FileTimeToQuadWord(&kernelTime);
+                    ULONGLONG procUser = FileTimeToQuadWord(&userTime);
+                    proc.cpuUsage = GetProcessCpu(pe32.th32ProcessID, procKernel, procUser, globalTimeDelta);
+                }
+                IO_COUNTERS io;
+                if (GetProcessIoCounters(hProcess, &io)) {
+                    proc.diskUsage = GetProcessDisk(pe32.th32ProcessID, io.ReadTransferCount, io.WriteTransferCount);
+                    proc.netUsage = GetRealNetworkUsage(proc, io, proc.lastUpdateTime);
+                    globalNetSpeed += proc.netUsage;
+                }
+                DWORD handleCount = 0;
+                if (GetProcessHandleCount(hProcess, &handleCount)) {
+                    globalTotalHandles += handleCount;
+                }
+                CloseHandle(hProcess);
+            }
+            processesCount++;
+        } while (Process32NextW(hSnapshot, &pe32));
+    }
+    CloseHandle(hSnapshot);
+
+    globalSystemUptime = GetTickCount64() / 1000;
+
+    for (INT i = 0; i < processesCount - 1; i++) {
+        for (INT j = i + 1; j < processesCount; j++) {
+            if (processes[i].category > processes[j].category ||
+                (processes[i].category == processes[j].category && processes[i].pid > processes[j].pid)) {
+                std::swap(processes[i], processes[j]);
+            }
+        }
+    }
+    return processesCount;
+}
