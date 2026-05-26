2669a0512aa586c9ef293cdacdad75b093a5871a
Ангелина Константинавичюс <Konstantinavi_AD_25@student.grsu.by>
2026-05-26 20:28:01 +0300

    Написан алгоритм группировки процессов BuildRenderItems
    Добавлена функция BuildRenderItems, которая принимает массив процессов бэкенда и формирует плоский список строк интерфейса. Интегрирован подсчет запущенных приложений, фоновых и системных служб

--- 2week.cpp
+++ 2week.cpp
@@ -29,3 +29,51 @@ void DrawLine(HANDLE hBuf, INT row, const std::string& text, WORD attributes) {
     WriteConsoleOutputCharacterA(hBuf, text.c_str(), (DWORD)text.length(), coord, &written);
     FillConsoleOutputAttribute(hBuf, attributes, consoleWidth, coord, &written);
 }
+void BuildRenderItems() {
+    renderItemsCount = 0;
+    int appsCount = 0, bgCount = 0, winCount = 0;
+    for (int i = 0; i < processesCount; i++) {
+        if (processes[i].category == CAT_APP) appsCount++;
+        else if (processes[i].category == CAT_BACKGROUND) bgCount++;
+        else winCount++;
+    }
+    EnsureRenderItemsCapacity(renderItemsCount + 1);
+    renderItems[renderItemsCount].isHeader = TRUE;
+    renderItems[renderItemsCount].text = "--- Приложения (" + std::to_string(appsCount) + ") ---";
+    renderItems[renderItemsCount].procIndex = -1;
+    renderItemsCount++;
+    for (int i = 0; i < processesCount; i++) {
+        if (processes[i].category == CAT_APP) {
+            EnsureRenderItemsCapacity(renderItemsCount + 1);
+            renderItems[renderItemsCount].isHeader = FALSE;
+            renderItems[renderItemsCount].procIndex = i;
+            renderItemsCount++;
+        }
+    }
+    EnsureRenderItemsCapacity(renderItemsCount + 1);
+    renderItems[renderItemsCount].isHeader = TRUE;
+    renderItems[renderItemsCount].text = "--- Фоновые процессы (" + std::to_string(bgCount) + ") ---";
+    renderItems[renderItemsCount].procIndex = -1;
+    renderItemsCount++;
+    for (int i = 0; i < processesCount; i++) {
+        if (processes[i].category == CAT_BACKGROUND) {
+            EnsureRenderItemsCapacity(renderItemsCount + 1);
+            renderItems[renderItemsCount].isHeader = FALSE;
+            renderItems[renderItemsCount].procIndex = i;
+            renderItemsCount++;
+        }
+    }
+    EnsureRenderItemsCapacity(renderItemsCount + 1);
+    renderItems[renderItemsCount].isHeader = TRUE;
+    renderItems[renderItemsCount].text = "--- Процессы Windows (" + std::to_string(winCount) + ") ---";
+    renderItems[renderItemsCount].procIndex = -1;
+    renderItemsCount++;
+    for (int i = 0; i < processesCount; i++) {
+        if (processes[i].category == CAT_WINDOWS) {
+            EnsureRenderItemsCapacity(renderItemsCount + 1);
+            renderItems[renderItemsCount].isHeader = FALSE;
+            renderItems[renderItemsCount].procIndex = i;
+            renderItemsCount++;
+        }
+    }
+}
