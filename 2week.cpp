d9c1a1083c2883e954cf3418cba23148f3370492
Ангелина Константинавичюс <Konstantinavi_AD_25@student.grsu.by>
2026-05-26 17:58:20 +0300

    Инициализация системы двойной буферизации консоли
    Добавлена функция InitDoubleBuffer, принимающая массив хэндлов и индекс текущего буфера по ссылке. Настроены жесткие размеры экранов, отключено стандартное мерцание консоли и скрыт системный мигающий курсор Windows

--- 2week.cpp
+++ 2week.cpp
@@ -1,1 +1,23 @@
+#include "common.h"
+RenderItem* renderItems = nullptr;
+int renderItemsCount = 0;
+int renderItemsCapacity = 0;
+HANDLE hBuffers[2];
+int currentBuffer = 0;
 
+void InitDoubleBuffer(int width, int height) {
+    for (int i = 0; i < 2; ++i) {
+        hBuffers[i] = CreateConsoleScreenBuffer(
+            GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+            NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+        COORD bufferSize = { static_cast<SHORT>(width), static_cast<SHORT>(height) };
+        SetConsoleScreenBufferSize(hBuffers[i], bufferSize);
+        SMALL_RECT windowSize = { 0, 0, static_cast<SHORT>(width - 1), static_cast<SHORT>(height - 1) };
+        SetConsoleWindowInfo(hBuffers[i], TRUE, &windowSize);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(hBuffers[i], &cursorInfo);
+        cursorInfo.bVisible = FALSE;
+        SetConsoleCursorInfo(hBuffers[i], &cursorInfo);
+    }
+    SetConsoleActiveScreenBuffer(hBuffers[currentBuffer]);
+}
